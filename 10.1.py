41c1eb28c11c378b3ac0fefc4bad7023b12d11e0
ROMKA49 <72030307+ROMKA49@users.noreply.github.com>
2020-10-11 19:23:05 +0300

    Add files via upload

---
+++ 10.1.py
@@ -0,0 +1,11 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def f(x):
+return 2*x+3
+a = 10
+b = 30
+while a < b:
+print (f(a))
+a= a + 2
+print("type 'end' to exit ")
+e=str(input())
