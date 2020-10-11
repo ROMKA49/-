bcc88369c60b8fd436321b897a7c67976d72912e
ROMKA49 <72030307+ROMKA49@users.noreply.github.com>
2020-10-11 18:58:03 +0300

    Add files via upload

---
+++ 9.3.py
@@ -0,0 +1,10 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> l = [3, 'hello', 7, 4, 'привет', 4, 3, -1]
+print (l[2:8])
+print (l[4:6])
+print (l[3])
+print (l[::2])
+print (l[1::2])
+print("type 'end' to exit ")
+e=str(input())
